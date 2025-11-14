20244ddcf02988d16c75e14f18a73ccd60fff72e
gmestanley <gmestanley@disroot.org>
2025-11-14 17:44:36 -0300

    Added RAMACCSTORE

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 sei ; Set interrupt disable flag
 cld ; Clear decimal mode (2A03 does not have it due to MOS patent) flag
-ldx #$ff ; Load 255 ($FF) into X register
+ldx #$FF	; Load 255 ($FF) into X register
 txs			; Transfer X to stack pointer
 lda #$00	; Load 0 ($00) into Accumulator
 sta $2000	; Store Accumulator (same as Load but doesn't set flags) into $2000
@@ -10,13 +10,14 @@ lda $2002	; Load Accumulator into $2002
 bpl BRANCH	; Branch back to BRANCH if negative flag clear
 ldy #$02	; Load 2 ($02) into Y register
 sty $01		; Store Y register into $01
-ldy #$00
-tya
-sta ($00), y
-iny
-bne $802b
-dec $01
-bpl $802b
+ldy #$00	; Load 0 ($00) into Y register
+tya			; Transfer Y register to Accumulator
+RAMACCSTORE:
+sta ($00), y; Store accumulator into CPURAM $0000
+iny			; Increment Y register
+bne RAMACCSTORE	; Branch to RAMACCSTORE if zero flag clear
+dec $01			; Decrement CPURAM $0001
+bpl RAMACCSTORE	; Branch to RAMACCSTORE if positive (negative flag clear)
 lda #$00
 sta $10
 lda #$00
@@ -31,7 +32,7 @@ lda #$00
 ldy #$00
 ldx #$03
 sta $2007
-dey
+dey				; Decrement the Y register
 bne $8052
 ldx #$81
 ldy #$76
